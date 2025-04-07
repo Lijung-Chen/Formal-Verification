f2041c1c899a3e6688224cb4f7e3f4342901bd4c
Lijung-Chen <lijung0979@gmail.com>
2025-04-07 16:47:03 +0800

    Add files via upload

---
+++ ISA Formal/source/trojan/biriscv/core/INCA_libs/irun.lnx86.15.20.nc/files.ts
@@ -0,0 +1,6 @@
+1698813425 /home/N26121729/FV/HW1/Project1_Group/source/trojan/biriscv/core/riscv_core.v
+1698815678 /home/N26121729/FV/HW1/Project1_Group/source/trojan/biriscv/core/biriscv_frontend.v
+1698813425 /home/N26121729/FV/HW1/Project1_Group/source/trojan/biriscv/core/biriscv_decode.v
+1698813425 /home/N26121729/FV/HW1/Project1_Group/source/trojan/biriscv/core/biriscv_decoder.v
+1698814155 /home/N26121729/FV/HW1/Project1_Group/source/trojan/biriscv/core/biriscv_npc.v
+1698816171 /home/N26121729/FV/HW1/Project1_Group/source/trojan/biriscv/core/biriscv_fetch.v
